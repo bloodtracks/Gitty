2b8b8a8b7a7952cdc210b4f703f6c52241e4efc1
Brenw <Brenw2@gmail.com>
2015-04-15 01:19:39 -0400

    created a working attempt at connecting to a data base through java but had no success in getting a login

---
+++ src/edu/bsu/cs346/JdbcCheckup2.java
@@ -0,0 +1,59 @@
+package edu.bsu.cs346;
+
+//You need to import the java.sql package to use JDBC
+import java.sql.*;
+
+//We import java.io to be able to read from the command line
+import java.io.*;
+
+//import oracle.jdbc.OracleDriver;
+
+class JdbcCheckup2
+{
+public static void main (String args [])
+    throws SQLException, IOException
+{
+ // Load the Oracle JDBC driver
+ DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+
+
+ String serverName = "csor6.dhcp.bsu.edu";
+ String portNumber = "1521";
+ String sid = "or6db";
+ String url ="jdbc:oracle:thin:@" + serverName + ":" + portNumber + ":" + sid;
+
+ Connection conn =
+ 	      DriverManager.getConnection (url,
+ 	           "bjwade", "1521");
+
+
+
+ System.out.println ("Oracle or6db is connected.");
+
+ // Create a statement
+ Statement stmt = conn.createStatement ();
+
+ // Do the SQL
+ ResultSet rset = stmt.executeQuery ("select sysdate from dual");
+
+ System.out.println("System time is: ");
+ while (rset.next ())
+   System.out.println (rset.getString (1));
+
+ System.out.println ("Your JDBC installation is correct.");
+
+ // close the resultSet
+ rset.close();
+
+ // Close the statement
+ stmt.close();
+
+ // Close the connection
+ conn.close();
+}
+
+}
+
+
+
+
