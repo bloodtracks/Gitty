1aee061a71b455d9f89be1d1592c1eb7b814f172
Brenw <Brenw2@gmail.com>
2015-04-16 01:06:24 -0400

    Created a VERY UGLY Swing GUI that displays the names of food items from the databse.

--- src/edu/bsu/cs346/JdbcCheckup2.java
+++ src/edu/bsu/cs346/JdbcCheckup2.java
@@ -2,11 +2,16 @@ package edu.bsu.cs346;
 
 //You need to import the java.sql package to use JDBC
 import java.sql.*;
-
+import java.util.ArrayList;
 //We import java.io to be able to read from the command line
 import java.io.*;
 
-//import oracle.jdbc.OracleDriver;
+
+
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 class JdbcCheckup2
 {
@@ -35,21 +40,37 @@ public static void main (String args [])
 
  System.out.println ("Your JDBC installation is correct.");
  
+
  
  
  ResultSet rset = stmt.executeQuery("SELECT * from FOOD_ITEM order by Price");
 
-
+  ArrayList<String> names = new ArrayList<String>();
+  ArrayList<String> prices = new ArrayList<String>();
  while(rset.next()){
+	String name = rset.getString(1);
+	String price = rset.getString(2);
+	 names.add(name);
+	 prices.add(price);
 	 System.out.println (rset.getString(1) + " Price: " + rset.getString(2));
  }
 
  
+ JFrame frame = new JFrame("Test Frame"); 
+ 
+ frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ frame.pack();
+ frame.setSize(200, 200);
+ frame.setVisible(true);
 	 
-
+ JPanel panel = new JPanel();
+ panel.add(new JLabel("Hello"));
+ frame.getContentPane().add(panel);
  
+ for(int i = 0; i< names.size(); i++){
+	 panel.add(new JLabel(names.get(i)) );
+ }
  
-
  // close the resultSet
  rset.close();
 
@@ -59,6 +80,9 @@ public static void main (String args [])
  // Close the connection
  conn.close();
 }
+  
+
+
 
 }
 
